0dbefbadcdb748df22534056b674c9b0fbcb91a2
Valentin Sarychev <dizel3d@gmail.com>
2022-01-15 19:35:55 +0300

    fix inspector

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import { Scene } from "@babylonjs/core/scene";
 import { Color3, Color4, MeshBuilder, DirectionalLight } from '@babylonjs/core';
 import { Vector3 } from '@babylonjs/core/Maths/math';
 import "@babylonjs/core/Meshes/meshBuilder";
-import "@babylonjs/inspector";
+import * as BABYLON from '@babylonjs/core';
+window["BABYLON"] = {...BABYLON};
+import("@babylonjs/inspector");
 //import "@babylonjs/gui";
 
 let c = document.getElementById("renderCanvas"); // Get the canvas element 
 let engine = new Engine(c, true); // Generate the BABYLON 3D engine
 let scene = new Scene(engine)
+scene.debugLayer.show();
 scene.clearColor = new Color4(0.02, 0.02, 0.02, 1);
 let VRHelper = scene.createDefaultVRExperience({ rayLength: 1000, laserToggle: false, useMultiview: false });
 VRHelper.enableInteractions();
